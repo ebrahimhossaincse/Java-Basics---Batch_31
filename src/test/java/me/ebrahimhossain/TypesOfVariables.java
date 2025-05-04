2df5b4a60419990a9a48c2465cf0d7d8ebedfa1a
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2025-05-04 12:43:04 -0400

    modify TypesOfVariable class

--- src/test/java/me/ebrahimhossain/TypesOfVariables.java
+++ src/test/java/me/ebrahimhossain/TypesOfVariables.java
@@ -5,6 +5,8 @@ public class TypesOfVariables {
     int b = 20; // Instance/non-static variable
     static int c = 50; //static/class variable
 
+    double d = 1.00;
+
     public static void main(String[] args) {
         int a = 5; // Local variable
     }
